79fcbf3f323ffc26d858377ee550a939e62b234d
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-04-18 18:32:20 -0400

    students array

---
+++ WK3/goal6_assign/js/main.js
@@ -0,0 +1,35 @@
+/*
+ * Goal 6 Mid-Term Assignment for PWA-1
+ * Duane Walters
+ * 04.18.2014
+ */
+
+//Self execution
+(function() {
+
+//Student Array
+    var students = [
+        {
+            name: "John Doe",
+            address: "123 Boring Lane, Somewhere Boring, ID",
+            gpa:[3.9,4.0,2.5,3.2],
+            date: getDate()
+
+        },
+
+        {
+            name: "Phillip Doe",
+            address: "123 Blech Dr., Bored, OK",
+            gpa:[1.7,2.8,3.9,4.0],
+            date: getDate()
+
+        },
+
+        {
+            name:"Karen Doe",
+            address:"123 Snooze Dr., Donut, MD",
+            gpa:[4.0,3.8,3.7,4.0],
+            date: getDate()
+
+        }
+    ];
